9b613c6ef43cf511a32ac69d6fc1d3857fd18ace
Alex <45183011+alexadams101@users.noreply.github.com>
2024-12-08 10:10:34 +0000

    refactor day_01

--- day_01/main.go
+++ day_01/main.go
@@ -1,42 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"bufio"
-	"strings"
+	"fmt"
+	"log"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Invalid number of args passed")
+	if len(os.Args) > 3 || len(os.Args) < 2 {
+		log.Fatalln("Invalid number of args passed")
 		return
 	}
 
-	data := os.Args[1]
+	filename := os.Args[1]
 
-	//read lists from file and and to array
-	file,err := os.Open(data)
-	if err != nil {
-		fmt.Printf("File %d does not exist", data)
+	if len(os.Args) > 2 {
+		part := os.Args[2]
+
+		if part == "1" {
+			part_1(filename)
+		} else if part == "2" {
+			part_2(filename)
+		} else {
+			log.Fatalln("Invalid part passed")
+		}
+	} else {
+		part_1(filename)
+		part_2(filename)
+	}
+}
+
+func part_1(filename string) {
+	array1, array2 := read_file(filename)
+
+	//sort them in asc order
+	array1 = quicksort(array1)
+	array2 = quicksort(array2)
+
+	//sum the differences of each entry in the arrays
+	sum := 0
+	for i := 0; i < len(array1); i++ {
+		num := array1[i] - array2[i]
+		if num < 0 {
+			num = num * -1
+		}
+		sum = sum + num
 	}
 
-	array1 := []int{}
-	array2 := []int{}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		values := strings.Split(scanner.Text(), "   ")
-		val1,_ := strconv.Atoi(values[0])
-		val2,_ := strconv.Atoi(values[1])
-		array1 = append(array1, val1)
-		array2 = append(array2, val2)
-	}
+	fmt.Println("part 1:", sum)
+}
 
-	//add values in array2 to map
+func part_2(filename string) {
+	array1, array2 := read_file(filename)
+
+	//add count of each number in array2 to a map
 	m := make(map[int]int)
-	for _,v := range array2 {
-		val,prs := m[v]
+	for _, v := range array2 {
+		val, prs := m[v]
 		if prs {
 			m[v] = val + 1
 		} else {
@@ -44,34 +67,38 @@ func main() {
 		}
 	}
 
-	//multiple each element by the count in array2
+	//multiple each element in array1 by the count in array2
 	result := 0
-	for _,v := range array1 {
-		val,prs := m[v]
+	for _, v := range array1 {
+		val, prs := m[v]
 		if prs {
-			result = result + v * val
+			result = result + v*val
 		}
 	}
-	fmt.Println(result)
 
-//	//sort them in asc order
-//	array1 = sort(array1)
-//	array2 = sort(array2)
-//
-//
-//	//for each entry in arrays sum the difference between the two
-//	result := 0
-//	for i:=0; i<len(array1); i++ {
-//		num := array1[i] - array2[i]
-//		if num < 0 {
-//			num = num * -1
-//		}
-//		result = result + num
-//	}
-//	fmt.Println(result)
+	fmt.Println("part 2:", result)
 }
 
-func sort(array []int) []int {
+func read_file(filename string) ([]int, []int) {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("File %d does not exist", filename)
+	}
+
+	array1 := []int{}
+	array2 := []int{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		values := strings.Split(scanner.Text(), "   ")
+		val1, _ := strconv.Atoi(values[0])
+		val2, _ := strconv.Atoi(values[1])
+		array1 = append(array1, val1)
+		array2 = append(array2, val2)
+	}
+	return array1, array2
+}
+
+func quicksort(array []int) []int {
 	if len(array) <= 1 {
 		return array
 	}
@@ -79,7 +106,7 @@ func sort(array []int) []int {
 	idx := -1
 	pivot := array[len(array)-1]
 
-	for i:=0; i<len(array)-1; i++ {
+	for i := 0; i < len(array)-1; i++ {
 		if array[i] <= pivot {
 			idx++
 			val := array[i]
@@ -91,8 +118,8 @@ func sort(array []int) []int {
 	array[len(array)-1] = array[idx]
 	array[idx] = pivot
 
-	left := sort(array[0:idx])
-	right := sort(array[idx+1:len(array)])
+	left := quicksort(array[0:idx])
+	right := quicksort(array[idx+1 : len(array)])
 
 	left = append(left, array[idx])
 	return append(left, right...)
